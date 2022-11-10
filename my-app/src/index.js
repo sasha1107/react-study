3c8d8ab2f2eed363e37df5261c58c45487cbfe55
Soohyun Jung <sasha1107@naver.com>
2022-11-10 23:29:55 +0900

    Refactor: Square 클래스를 함수 컴포넌트로 수정

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,25 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
-class Square extends React.Component {
-    // constructor(props){
-    //     // JavaScript 클래스에서 하위 클래스의 생성자를 정의할 때 항상 super를 호출해야합니다. 모든 React 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출 구문부터 작성해야 합니다.
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     };
-    // }
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => 
-                    this.props.onClick()
-                }>
-            {this.props.value}
-            </button>
-        );
-    }
+
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
