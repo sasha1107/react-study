7554ecc71e931ad2cc828b80d42aec305a8ecba0
Soohyun Jung <sasha1107@naver.com>
2022-11-11 00:18:00 +0900

    Feat: 승자 결정하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -25,6 +25,12 @@ class Board extends React.Component {
     handleClick(i){
         // console.log(this.state.squares);
         const squares = this.state.squares.slice(); // 복사
+
+        // 누군가가 승리하거나 Square가 이미 채워졌다면 Board의 handleClick 함수가 클릭을 무시
+        if (calculateWinner(squares) || squares[i]){{
+            return;
+        }}
+
         squares[i] = this.state.xIsNext ? 'X': 'O';
         this.setState({
             squares: squares,
@@ -42,7 +48,14 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner){
+            status = "Winner: " + winner
+        }
+        else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+        }
 
         return (
             <div>
@@ -81,6 +94,26 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2], // 가로 1번째 줄
+        [3, 4, 5], // 가로 2번째 줄
+        [6, 7, 8], // 가로 3번째 줄
+        [0, 3, 6], // 세로 1번째 줄
+        [1, 4, 7], // 세로 2번째 줄
+        [2, 5, 8], // 세로 3번째 줄
+        [0, 4, 8], // 대각선 \
+        [2, 4, 6], // 대각선 /
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
