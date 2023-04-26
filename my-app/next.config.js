e7d607f6a07e951dd6dab7b626bf622f5ff94b61
정수현 <sasha1107@naver.com>
2023-04-26 16:31:13 +0900

    Config tsconfig, next.config
    - SVG 사용 설정
    - path 설정

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -23,6 +23,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
 }
 module.exports = withMDX(nextConfig)
 
--- my-app/src/app/Header.tsx
+++ my-app/src/app/Header.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import { Text } from '@chakra-ui/react';
+import Logo from '@assets/react.svg'
 
 export default function Header() {
     return (
-        <div className='w-screen bg-black p-4 sticky top-0 z-50'>
-            <Text fontSize={"xl"} className='text-[#149ECA]'>
+        <div className='w-screen bg-black p-4 sticky top-0 z-50 flex items-center gap-2'>
+            <Logo width='30' height='30' className='inline animate-bounce  hover:animate-ping transition'/>
+            <Text fontSize={"xl"} className='text-[#00D8FF]'>
                 정수현의 React Beta
             </Text>
         </div>
