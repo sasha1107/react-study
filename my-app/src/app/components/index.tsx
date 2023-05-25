1721be795b9036a7a86f015630c963a7ef87f653
정수현 <sasha1107@naver.com>
2023-05-25 15:17:45 +0900

    Refactor: 컴포넌트import 간소화

---
+++ my-app/src/app/components/index.tsx
@@ -0,0 +1,8 @@
+export { DeepDive } from './DeepDive';
+export { Highlight } from './Highlight';
+export { CodeBlock } from './CodeBlock';
+export { Pitfall } from './Pitfall';
+export { RedCard } from './RedCard';
+export { WhiteCard } from './WhiteCard';
+export { ColorCard }from './ColorCard';
+export { AlertBanner }from './AlertBanner';
