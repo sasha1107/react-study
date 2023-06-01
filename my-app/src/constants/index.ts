31e3baa68d9a2e2595fe9af21b2dd6f00bece234
Soohyun Jung <sasha1107@naver.com>
2023-06-01 23:48:50 +0900

    Refactor: 내비게이션 바 리팩토링

---
+++ my-app/src/constants/index.ts
@@ -0,0 +1,185 @@
+const navbarData = [
+    {
+        category: 'Describing the UI',
+        children: [
+            {
+                title: '컴포넌트 생성',
+                href: '../../describing-the-ui/your-first-component',
+            },
+            {
+                title: '컴포넌트 import, export',
+                href: '../../describing-the-ui/importing-and-exporting-components',
+            },
+            {
+                title: 'JSX로 마크업',
+                href: '../../describing-the-ui/writing-markup-with-jsx',
+            },
+            {
+                title: '중괄호로 자바스크립트 사용',
+                href: '../../describing-the-ui/javascript-in-jsx-with-curly-braces',
+            },
+            {
+                title: '컴포넌트에 props 넘기기',
+                href: '../../describing-the-ui/passing-props-to-a-component',
+            },
+            {
+                title: '조건부 렌더링',
+                href: '../../describing-the-ui/conditional-rendering',
+            },
+            {
+                title: '리스트를 렌더링하기',
+                href: '../../describing-the-ui/rendering-lists',
+            },
+            {
+                title: '컴포넌트를 순수하게 유지',
+                href: '../../describing-the-ui/keeping-components-pure',
+            },
+        ],
+    },
+    {
+        category: 'Adding Interactivity',
+        children: [
+            {
+                title: '이벤트에 반응하기',
+                href: '../../adding-interactivity/responding-to-events',
+            },
+            {
+                title: 'state: 컴포넌트의 memory',
+                href: '../../adding-interactivity/state-a-components-memory',
+            },
+            {
+                title: '렌더와 커밋',
+                href: '../../adding-interactivity/render-and-commit',
+            },
+            {
+                title: '스냅샷으로서의 state',
+                href: '../../adding-interactivity/state-as-a-snapshot',
+            },
+            {
+                title: '일련의 state 업데이트 대기열',
+                href: '../../adding-interactivity/queueing-a-series-of-state-updates',
+            },
+            {
+                title: '객체 state 업데이트',
+                href: '../../adding-interactivity/updating-objects-in-state',
+            },
+            {
+                title: '배열 state 업데이트',
+                href: '../../adding-interactivity/updating-arrays-in-state',
+            },
+        ],
+    },
+    {
+        category: 'Managing State',
+        children: [
+            {
+                title: 'state로 입력에 반응하기',
+                href: '../../managing-state/reacting-to-input-with-state',
+            },
+            {
+                title: 'choosing-the-state-structure',
+                href: '../../managing-state/choosing-the-state-structure',
+            },
+            {
+                title: 'sharing-state-between-components',
+                href: '../../managing-state/sharing-state-between-components',
+            },
+            {
+                title: 'preserving-and-resetting-state',
+                href: '../../managing-state/preserving-and-resetting-state',
+            },
+            {
+                title: 'extracting-state-logic-into-a-reducer',
+                href: '../../managing-state/extracting-state-logic-into-a-reducer',
+            },
+            {
+                title: 'passing-data-deeply-with-context',
+                href: '../../managing-state/passing-data-deeply-with-context',
+            },
+            {
+                title: 'scaling-up-with-reducer-and-context',
+                href: '../../managing-state/scaling-up-with-reducer-and-context',
+            },
+        ]
+    },
+    {
+        category: 'Escape Hatches',
+        children: [
+            {
+                title: 'Referencing Values with Refs',
+                href: '../../escape-hatches/referencing-values-with-refs',
+            },
+            {
+                title: 'Manipulating the DOM with Refs',
+                href: '../../escape-hatches/manipulating-the-dom-with-refs',
+            },
+            {
+                title: 'Synchronizing with Effects',
+                href: '../../escape-hatches/synchronizing-with-effects',
+            },
+            {
+                title: 'You Might Not Need an Effect',
+                href: '../../escape-hatches/you-might-not-need-an-effect',
+            },
+            {
+                title: 'Lifecycle of Reactive Effects',
+                href: '../../escape-hatches/lifecycle-of-reactive-effects',
+            },
+            {
+                title: 'Separating Events from Effects',
+                href: '../../escape-hatches/separating-events-from-effects',
+            },
+            {
+                title: 'Removing Effect Dependencies',
+                href: '../../escape-hatches/removing-effect-dependencies',
+            },
+            {
+                title: 'Reusing Logic with Custom Hooks',
+                href: '../../escape-hatches/reusing-logic-with-custom-hooks',
+            }
+        ]
+    },
+    {
+        category: 'Custom Hooks',
+        children: [
+            {
+                title: 'useWindowSize',
+                href: '../../custom-hooks/useWindowSize',
+            },
+            {
+                title: 'useToggle',
+                href: '../../custom-hooks/useToggle',
+            },
+            {
+                title: 'useLockBodyScroll',
+                href: '../../custom-hooks/useLockBodyScroll',
+            },
+            {
+                title: 'useKeyPress',
+                href: '../../custom-hooks/useKeyPress',
+            },
+            {
+                title: 'useOnClickOutside',
+                href: '../../custom-hooks/useOnClickOutside',
+            },
+            {
+                title: 'useHover',
+                href: '../../custom-hooks/useHover',
+            },
+            {
+                title: 'useAxios',
+                href: '../../custom-hooks/useAxios',
+            },
+            {
+                title: 'useLocalStorage',
+                href: '../../custom-hooks/useLocalStorage',
+            },
+            {
+                title: 'useTitle',
+                href: '../../custom-hooks/useTitle',
+            }
+        ]
+    }
+];
+
+export { navbarData };
