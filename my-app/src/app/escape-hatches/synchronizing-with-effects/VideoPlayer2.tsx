547f422da30bbc169c88eafe0da7a6bd96016a76
정수현 <sasha1107@naver.com>
2023-06-02 14:22:49 +0900

    Feat: Synchronizing with Effects 중간 저장

--- my-app/src/app/escape-hatches/synchronizing-with-effects/VideoPlayer2.tsx
+++ my-app/src/app/escape-hatches/synchronizing-with-effects/VideoPlayer2.tsx
@@ -5,8 +5,10 @@ function VideoPlayer({ src, isPlaying } : { src : string, isPlaying : boolean })
 
     useEffect(() => {
         if (isPlaying) {
+            console.log('Calling video.play()');
             ref.current?.play();
         } else {
+            console.log('Calling video.pause()');
             ref.current?.pause();
         }
     });
@@ -14,10 +16,12 @@ function VideoPlayer({ src, isPlaying } : { src : string, isPlaying : boolean })
     return <video ref={ref} src={src} loop playsInline />;
 }
 
-export default function VideoPlayerApp() {
+export default function App() {
     const [isPlaying, setIsPlaying] = useState(false);
+    const [text, setText] = useState('');
     return (
         <>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
             <button onClick={() => setIsPlaying(!isPlaying)} className='border-2 border-indigo-400 bg-indigo-100 hover:bg-indigo-200'>
                 {isPlaying ? 'Pause' : 'Play'}
             </button>
