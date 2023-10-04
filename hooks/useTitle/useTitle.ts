fb6725cce0997c28461793543cfe97912041af21
정수현 <sasha1107@naver.com>
2023-10-04 13:15:24 +0900

    Fix: useTitle dependency

--- hooks/useTitle/useTitle.ts
+++ hooks/useTitle/useTitle.ts
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 
 const useTitle = (title: string) => {
-    useEffect(() => {
-        const prevTitle = document.title;
-        document.title = title;
+  useEffect(() => {
+    const prevTitle = document.title;
+    document.title = title;
 
-        return () => (document.title = prevTitle);
-    }, [title]);
+    return () => (document.title = prevTitle);
+  }, []);
 };
 
 export default useTitle;
